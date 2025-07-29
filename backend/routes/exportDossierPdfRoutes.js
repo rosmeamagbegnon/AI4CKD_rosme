d4fd7c6a73da5cee680ae91a18b4280d8ca8adbe
rosmeamagbegnon <rosmeamagbegnon@gmail.com>
2025-07-29 22:14:48 +0100

    deletion

--- backend/routes/exportDossierPdfRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const exportController = require('../controllers/exportDossierPdfController');
-
-router.get('/patients/:id/export-pdf', exportController.exportDossierPdf);
-
-module.exports = router;
